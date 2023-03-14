2476e1c5b6232f7b21d65ab6f57ab7d9009d2866
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-03-14 17:04:11 +0000

    Add media query for header

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const menuIcon = document.querySelector('.hamburgur');
-menuIcon.addEventListener('click', () => {
-  document.querySelector('.mobile-nav').style.display = 'flex';
-  document.body.style.overflow = 'hidden';
+const menuIcon = document.querySelectorAll('.hamburgur');
+
+menuIcon.forEach((icon) => {
+  icon.addEventListener('click', () => {
+    document.querySelector('.mobile-nav').style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+  });
 });
+
+// menuIcon.addEventListener('click', () => {
+//   document.querySelector('.mobile-nav').style.display = 'flex';
+//   document.body.style.overflow = 'hidden';
+// });
 
 const closeMenu = () => {
   document.querySelector('.mobile-nav').style.display = 'none';
